7ae04e76b0db8dd2366e379c8e8d7862a50d7d6a
partyrico <cjmoynihan@gmail.com>
2018-03-03 16:36:54 -0500

    Added the ability to login, with a simple mock landing page

--- web_handling.py
+++ web_handling.py
@@ -37,29 +37,38 @@ def close_db(error):
     get_db().conn.close()
 
 # Actual web handling
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+        if request.form['action'] == 'Login':
+            if request.form['username'] not in get_db().c.execute("SELECT username FROM users").fetchall():
+                error = 'Invalid username'
+            elif request.form['password'] not in get_db().c.execute("SELECT password FROM users").fetchall():
+                error = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                flash('You were logged in')
+                return redirect(url_for('show_calendar'))
+        elif request.form['action'] == 'Add User':
+            if request.form['username'] in get_db().c.execute("SELECT username FROM users").fetchall():
+                error = 'Invalid username'
+            else:
+                session['logged_in'] = True
+                flash('User added. You were logged in')
+                return redirect(url_for('show_calendar'))
+        return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('login'))
 
-@app.route('/')
-def test_page():
-    return 'Hello World!'
+@app.route('/show_calendar')
+def show_calendar():
+    return "Hello World! We did it!"
 
 # @app.route('/add', methods=['POST'])
 # def add_entry():
