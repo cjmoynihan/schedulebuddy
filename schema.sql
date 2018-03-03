decc3a9b0ba87d45a9d182bf16c76c38cc0493d8
partyrico <cjmoynihan@gmail.com>
2018-03-03 16:16:36 -0500

    Fixed the autoincrement for the database

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 create table if not exists users(
-    user_id INTEGER primary key auto_increment,
+    user_id INTEGER primary key autoincrement,
     username text not null,
     password text not null
 );
 create table if not exists events(
-    event_id INTEGER primary key auto_increment,
+    event_id INTEGER primary key autoincrement,
     event_name text not null,
     user_id int not null,
     start_time int not null,
