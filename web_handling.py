cda949d93f6a6bb1d3612133057ed252e9c50111
partyrico <cjmoynihan@gmail.com>
2018-03-04 14:31:35 -0500

    Added new web handling tools

--- web_handling.py
+++ web_handling.py
@@ -7,58 +7,58 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, make_response
 from functools import update_wrapper
 
-import datetime
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
-                attach_to_all=True, automatic_options=True):
-    """Decorator function that allows crossdomain requests.
-      Courtesy of
-      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
-    """
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, list):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, list):
-        origin = ', '.join(origin)
-    if isinstance(max_age, datetime.timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        """ Determines which methods are allowed
-        """
-        if methods is not None:
-            return methods
-
-        options_resp = app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        """The decorator function
-        """
-        def wrapped_function(*args, **kwargs):
-            """Caries out the actual cross domain code
-            """
-            if automatic_options and request.method == 'OPTIONS':
-                resp = app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+# import datetime
+# def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
+#                 attach_to_all=True, automatic_options=True):
+#     """Decorator function that allows crossdomain requests.
+#       Courtesy of
+#       https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
+#     """
+#     if methods is not None:
+#         methods = ', '.join(sorted(x.upper() for x in methods))
+#     if headers is not None and not isinstance(headers, list):
+#         headers = ', '.join(x.upper() for x in headers)
+#     if not isinstance(origin, list):
+#         origin = ', '.join(origin)
+#     if isinstance(max_age, datetime.timedelta):
+#         max_age = max_age.total_seconds()
+#
+#     def get_methods():
+#         """ Determines which methods are allowed
+#         """
+#         if methods is not None:
+#             return methods
+#
+#         options_resp = app.make_default_options_response()
+#         return options_resp.headers['allow']
+#
+#     def decorator(f):
+#         """The decorator function
+#         """
+#         def wrapped_function(*args, **kwargs):
+#             """Caries out the actual cross domain code
+#             """
+#             if automatic_options and request.method == 'OPTIONS':
+#                 resp = app.make_default_options_response()
+#             else:
+#                 resp = make_response(f(*args, **kwargs))
+#             if not attach_to_all and request.method != 'OPTIONS':
+#                 return resp
+#
+#             h = resp.headers
+#             h['Access-Control-Allow-Origin'] = origin
+#             h['Access-Control-Allow-Methods'] = get_methods()
+#             h['Access-Control-Max-Age'] = str(max_age)
+#             h['Access-Control-Allow-Credentials'] = 'true'
+#             h['Access-Control-Allow-Headers'] = \
+#                 "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+#             if headers is not None:
+#                 h['Access-Control-Allow-Headers'] = headers
+#             return resp
+#
+#         f.provide_automatic_options = False
+#         return update_wrapper(wrapped_function, f)
+#     return decorator
 
 def jsonify(*args, **kwargs):
     from flask import jsonify
@@ -159,11 +159,17 @@ def add_event():
         return str(e)
     return "Added event successfully!!"
 
-@app.route('/get_events/<username>', methods=['OPTIONS'])
-@crossdomain(origin='*')
+@app.route('/get_events/<username>')
 def get_events(username):
     try:
         return jsonify(get_db().get_sorted_events(username))
+    except ValueError as e:
+        return str(e)
+
+@app.route('/get_events', methods=['GET', 'POST'])
+def fixed_get_events():
+    try:
+        return jsonify(get_db().get_sorted_events(request.args['username']))
     except ValueError as e:
         return str(e)
 
@@ -178,11 +184,17 @@ def add_friend():
     except ValueError as e:
         return str(e)
 
-@app.route('/get_friends/<username>', methods=['OPTIONS'])
-@crossdomain(origin='*')
+@app.route('/get_friends/<username>')
 def get_friends(username):
     try:
         return jsonify(get_db().get_friends(username))
+    except ValueError as e:
+        return str(e)
+
+@app.route('/get_friends', methods=['GET','POST'])
+def fixed_get_friends():
+    try:
+        return jsonify(get_db().get_friends(request.args['username']))
     except ValueError as e:
         return str(e)
 
